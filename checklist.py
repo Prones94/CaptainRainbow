5945474892da90e1d1b29d957b03535436eee895
Ian Rones <ronesp.85@gmail.com>
2019-08-29 23:24:47 -0700

    Finished checklist, added in stretch challenge

--- checklist.py
+++ checklist.py
@@ -37,31 +37,36 @@ def mark_completed(index):
 def select(function_code):
     # Create item
     if function_code == 'A':
-        item = user_input('What do you want to add:  ')
+        item = user_input(Fore.LIGHTBLUE_EX + 'What do you want to add:  ' + Fore.RESET)
         create(item)
 
     # Read item
     elif function_code == 'R':
-        item_index = user_input('Index Number: ')
+        item_index = user_input(Fore.LIGHTMAGENTA_EX + 'Index Number: ' + Fore.RESET)
         read(int(item_index))
     
     # Destroy item
     elif function_code == 'D':
-        index = user_input('Index Number: ')
+        index = user_input(Fore.LIGHTGREEN_EX + 'Index Number: ' + Fore.RESET)
         destroy(int(index))
 
     # Print all items
     elif function_code == 'P':
         for item in checklist:
-            print(item)
+            print(Fore.LIGHTRED_EX + item + Fore.RESET)
         
+    # Mark completed items w/ √
+    elif function_code == 'U':
+        index = user_input('Index Number: ')
+        mark_completed(int(index))
 
+    # Quit program 
     elif function_code == 'Q':
         return False
     
     # Catch all
     else:
-        print('Unknown Option')
+        print(Fore.YELLOW + 'Unknown Option' + Fore.RESET)
     return True
 
 #USER INPUT function
@@ -96,5 +101,5 @@ def test():
 
 running = True
 while running:
-    selection = user_input('Press A to add to list, R to remove from list, P to print list, and Q to quit: ')
+    selection = user_input('Press A to add to list, D to remove from list, U to mark items completed, R to read from the list, P to print list, and Q to quit: ')
     running = select(selection)
